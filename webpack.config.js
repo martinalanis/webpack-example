4a07f4d1a4608caca0bac0b65e555c821fc8f93a
Martin Alanis <martinalanis.dev@gmail.com>
2023-01-30 12:45:44 -0600

    tags param

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,15 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
+      title: "Webpack Test",
       templateParameters: {
-        title: "Webpack Test",
         PUBLIC_URL: "demo/value",
-        htmlWebpackPlugin: {
-          tags: [
+        tags: [
             {
               tagName: "script",
               attributes: { src: "https://l360.ahs.com/script.js" }
             }
           ]
-        }
       }
     })
   ]
