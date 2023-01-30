e8e6eafd9ea9c35b2e774ed447c6f1cc8d0a44b6
Martin Alanis <martinalanis.dev@gmail.com>
2023-01-30 12:30:44 -0600

    headTag test

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,11 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       templateParameters: {
-        PUBLIC_URL: "demo/value"
+        PUBLIC_URL: "demo/value",
+        headTags: {
+          tagName: "script",
+          attributes: { src: "https://l360.ahs.com/script.js" }
+        }
       }
     })
   ]
