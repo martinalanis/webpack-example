2dc2bd9cc416af03ebf3b9bc6d4f8303de154a30
Martin Alanis <martinalanis.dev@gmail.com>
2023-01-30 12:19:47 -0600

    remove tag

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       templateParameters: {
-        PUBLIC_URL: "demo/value",
-        tags: {
-          headTags: '<script src="https://l360.ahs.com/script.js"></script>'
-        }
+        PUBLIC_URL: "demo/value"
       }
     })
   ]
